2644bf25b88d3815f2850ef1be30679f7a3bcecd
Md Saniul Basir Saz <96694508+mdsaniulbasirsaz@users.noreply.github.com>
2023-03-25 10:54:54 +0600

    Added solution - LeetHub

---
+++ Reverse digits - GFG/reverse-digits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int reverse_digit(long long int n)
+		{
+		    long long ans=0;
+		    long long i=0;
+		    while(n>0)
+		    {
+		        long long r=n%10;
+		        ans=ans*10+r;
+		        n/=10;
+		    }
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.reverse_digit(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
